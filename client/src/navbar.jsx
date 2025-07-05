de73415cdb993c686600fe0bb6f693e2aa468009
shab1b1 <240359r@mymail.nyp.edu.sg>
2025-07-05 23:55:21 +0800

    Update navbar.jsx

--- client/src/navbar.jsx
+++ client/src/navbar.jsx
@@ -3,9 +3,9 @@ import './Navbar.css';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">DesCode</div>
+      <a className="navbar-logo" href="/">DesCode</a>
       <ul className="navbar-links">
-        <li><a href="#">Search</a></li>
+        <li><a href="/cop.js">Search</a></li>
         <li><a href="#">About Us</a></li>
         <li><a href="#">Contact Us</a></li>
         <li><a className="signup-btn" href="#">Sign Up</a></li>
